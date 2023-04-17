84436660f50af66458b46fcb76a72e7147b956b8
Kevin Hermann <hermannkevin@gmail.com>
2023-04-17 16:23:00 +0200

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,7 +1,9 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.color_scheme = 'Solarized (light) (terminal.sexy)'
+-- config.color_scheme = 'Solarized (light) (terminal.sexy)'
+config.color_scheme = 'Default Dark (base16)'
+
 config.font = wezterm.font 'JetBrains Mono'
 
 return config
