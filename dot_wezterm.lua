14bf2cc7766162549db3d93f8e0c6d063a018b49
Kevin Hermann <hermannkevin@gmail.com>
2023-04-23 16:41:14 +0200

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,10 +1,17 @@
 local wezterm = require 'wezterm'
 local config = {}
 
--- config.color_scheme = 'Solarized (light) (terminal.sexy)'
 config.color_scheme = 'Solarized (light) (terminal.sexy)'
-
-
 config.font = wezterm.font 'JetBrains Mono'
+config.window_close_confirmation = 'NeverPrompt'
+
+config.keys = {
+  {
+    key = 'w',
+    mods = 'SHIFT | CTRL',
+    action = wezterm.action.CloseCurrentTab { confirm = false },
+  },
+}
+
 
 return config
