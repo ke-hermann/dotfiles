3adbe9bc7de036706838ca35612edb4e441d6332
Kevin Hermann <hermannkevin@gmail.com>
2023-04-25 16:09:23 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -106,12 +106,15 @@ lsp.setup()
 
 -- CMP
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
 	mapping = {
-		['<CR>'] = cmp.mapping.confirm({ select = false }),
+		['<Tab>'] = cmp_action.luasnip_supertab(),
+		['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
 	}
 })
+
 
 -- Autopair
 require("nvim-autopairs").setup()
