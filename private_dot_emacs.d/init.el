bb7da733787892cac65aa2fdb22a420f38757e5f
Kevin Hermann <hermannkevin@gmail.com>
2023-06-19 07:33:40 +0200

    Update .config/fish/conf.d/abbr.fish Update .emacs.d/init.el

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -195,7 +195,6 @@
 
 ;; optionally if you want to use debugger
 (use-package dap-mode)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;; COMPLETION STACK ;;
