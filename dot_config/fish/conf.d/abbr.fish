c8434b2986952a4b13736b5fc6c9a2f0356baf1b
Kevin Hermann <hermannkevin@gmail.com>
2023-06-20 09:11:59 +0200

    Update .config/fish/conf.d/abbr.fish Add .config/fish/conf.d/theme.fish

--- dot_config/fish/conf.d/abbr.fish
+++ dot_config/fish/conf.d/abbr.fish
@@ -1,10 +1,16 @@
-# pacman abbrevations
+# pacman abbreviations
 abbr -a 'pci' 'sudo pacman -S'
 abbr -a 'pcu' 'sudo pacman -Syu'
 abbr -a 'pcs' 'sudo pacman -Ss'
 
-# apt  abbrevations
+# apt  abbreviations
 abbr -a 'agi' 'sudo apt install -y'
 abbr -a 'agr' 'sudo apt remove'
 abbr -a 'aguu' 'sudo apt update && sudo apt upgrade -y'
 abbr -a 'agar' 'sudo apt autoremove'
+
+# dnf abbreviations
+
+abbr -a 'dnfu' 'sudo dnf install -y'
+abbr -a 'dnfi' 'sudo dnf remove'
+abbr -a 'dnfr' 'sudo dnf upgrade'
