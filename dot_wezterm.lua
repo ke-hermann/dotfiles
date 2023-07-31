76c36de7e33fbfdafe75dbb7dd30a200addf147a
Kevin Hermann <hermannkevin@gmail.com>
2023-07-31 04:12:46 +0200

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,14 +1,16 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font 'Berkeley Mono'
-config.font_size = 14
+config.font = wezterm.font 'JetBrains Mono'
 config.window_close_confirmation = 'NeverPrompt'
 config.warn_about_missing_glyphs = false
 
 -- Spawn powershell on windows
 if package.config:sub(1, 1) == '\\' then
 	config.default_prog = { 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe'}
+	config.font_size = 12
+else
+	config.font_size = 14
 end
 
 config.color_scheme = 'Default Dark (base16)'
