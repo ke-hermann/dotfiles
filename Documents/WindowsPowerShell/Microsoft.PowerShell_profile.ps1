042a2586cfd20361d025db35eb92bc50dcf2f9cb
Kevin Hermann <hermannkevin@gmail.com>
2023-08-02 03:40:26 +0200

    Update Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1 Change attributes of .config/zsh-abbr/user-abbreviations

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,31 +1,31 @@
 Invoke-Expression (&starship init powershell)
 $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
 
-# Set-PSReadLineOption -Colors @{
-#   Command            = 'Black'
-#   Number             = 'DarkGray'
-#   Member             = 'DarkGray'
-#   Operator           = 'DarkGray'
-#   Type               = 'DarkGray'
-#   Variable           = 'DarkGreen'
-#   Parameter          = 'DarkGreen'
-#   ContinuationPrompt = 'DarkGray'
-#   Default            = 'DarkGray'
-# }
+Set-PSReadLineOption -Colors @{
+  Command            = 'Black'
+  Number             = 'DarkGray'
+  Member             = 'DarkGray'
+  Operator           = 'DarkGray'
+  Type               = 'DarkGray'
+  Variable           = 'DarkGreen'
+  Parameter          = 'DarkGreen'
+  ContinuationPrompt = 'DarkGray'
+  Default            = 'DarkGray'
+}
  
-# # Logging / Progress colors
+# Logging / Progress colors
  
-# $p = $host.privatedata
-# $p.ErrorForegroundColor    = "Red"
-# $p.ErrorBackgroundColor    = "White"
-# $p.WarningForegroundColor  = "Yellow"
-# $p.WarningBackgroundColor  = "White"
-# $p.DebugForegroundColor    = "Yellow"
-# $p.DebugBackgroundColor    = "White"
-# $p.VerboseForegroundColor  = "Black"
-# $p.VerboseBackgroundColor  = "White"
-# $p.ProgressForegroundColor = "DarkGray"
-# $p.ProgressBackgroundColor = "White"
+$p = $host.privatedata
+$p.ErrorForegroundColor    = "Red"
+$p.ErrorBackgroundColor    = "White"
+$p.WarningForegroundColor  = "Yellow"
+$p.WarningBackgroundColor  = "White"
+$p.DebugForegroundColor    = "Yellow"
+$p.DebugBackgroundColor    = "White"
+$p.VerboseForegroundColor  = "Black"
+$p.VerboseBackgroundColor  = "White"
+$p.ProgressForegroundColor = "DarkGray"
+$p.ProgressBackgroundColor = "White"
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
