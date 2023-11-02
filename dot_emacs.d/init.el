af3f1645c9d4a6529af3ccfa190fd68230ef4039
Kevin Hermann <hermannkevin@gmail.com>
2023-11-02 14:02:28 +0100

    update
    new emacs theme
    
    Update .config/starship.toml
    Update .wezterm.lua
    
    Add .emacs.d/signalis-theme.el
    
    Update .emacs.d/signalis-theme.el
    
    Update .emacs.d/signalis-theme.el
    
    Update .emacs.d/signalis-theme.el
    
    Update .emacs.d/init.el
    Update .emacs.d/signalis-theme.el

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -77,15 +77,6 @@
   :diminish company-mode
   :config (global-company-mode +1))
 
-(use-package tree-sitter
-  :ensure t
-  :config
-  (global-tree-sitter-mode +1)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
-
-(use-package tree-sitter-langs
-  :ensure t
-  :after tree-sitter)
 
 (use-package flycheck
   :ensure t
--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,14 +4,24 @@ local config = {}
 config.window_close_confirmation = 'NeverPrompt'
 config.warn_about_missing_glyphs = false
 
-config.font = wezterm.font 'Cascadia Code'
+config.font = wezterm.font 'Geist Mono'
 config.font_size = 16
 
-config.color_scheme = 'Tomorrow Night Burns'
+config.color_scheme = "Tomorrow Night Burns"
 
 config.use_fancy_tab_bar = false
 
 config.hide_tab_bar_if_only_one_tab = true
+
+-- Spawn powershell on windows
+if package.config:sub(1, 1) == '\\' then
+   config.default_prog = { 'powershell'}
+   config.font = wezterm.font 'Cascadia Code'
+   config.font_size = 12
+else
+   config.font = wezterm.font 'JetBrains Mono'
+   config.font_size = 16
+end
 
 config.keys = {
    {
