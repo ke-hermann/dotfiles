b4be474335c1f282510b0f3a40cb55bb20d7d6d2
Kevin Hermann <hermannkevin@gmail.com>
2023-12-18 03:25:59 +0100

    Update .emacs.d/init.el

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -46,21 +46,23 @@
 (setq auto-revert-verbose nil)
 (show-paren-mode +1)
 
-(defvar use-package-always-ensure)
 (setq use-package-always-ensure t)
 
 ;; Packages
 
 (use-package diminish
+  :ensure t
   :config
   (diminish 'eldoc-mode))
 
 ;; This package implements suppor for mapping a pair of simultaneously pressed keys .
 (use-package key-chord
+  :ensure t
   :config (key-chord-mode 1))
 
 ;; super-save auto-saves your buffers
 (use-package super-save
+  :ensure t
   :diminish super-save-mode
   :config
   (super-save-mode +1))
@@ -71,81 +73,112 @@
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
 
-(use-package magit)
+(use-package magit
+  :ensure t)
 
 (use-package whole-line-or-region
+  :ensure t
   :diminish (whole-line-or-region-global-mode whole-line-or-region-local-mode)
   :config (whole-line-or-region-global-mode +1))
 
 (use-package company
+  :ensure t
   :diminish company-mode
   :config (global-company-mode +1))
 
 
 (use-package flycheck
+  :ensure t
   :config (global-flycheck-mode))
 
 (use-package which-key
+  :ensure t
   :diminish which-key-mode
   :config (which-key-mode +1))
 
 (use-package ace-window
+  :ensure t
   :bind (("M-o" . ace-window)))
 
 (use-package cider
+  :ensure t
   :config
-  (add-hook 'cider-repl-mode-hook (lambda () (display-line-numbers-mode -1))))
+  (add-hook 'cider-repl-mode-hook (lambda () (display-line-numbers-mode -1)))
+  (add-hook 'cider-repl-mode-hook #'enable-paredit-mode))
 
 (use-package paredit
+  :ensure t
   :diminish paredit-mode
   :config
   (add-hook 'clojure-mode-hook #'enable-paredit-mode)
   (add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode))
 
-(use-package nerd-icons)
+(use-package nerd-icons
+  :ensure t)
 
 (use-package doom-modeline
+  :ensure t
   :after (nerd-icons)
   :config
   (setq column-number-mode +1)
   (setq doom-modeline-icon nil)
   (doom-modeline-mode +1))
 
-(use-package doom-themes)
-(use-package ef-themes)
-(use-package solarized-theme)
+(use-package doom-themes
+  :ensure t)
+(use-package ef-themes
+  :ensure t)
+(use-package solarized-theme
+  :ensure t)
 
 (use-package projectile
+  :ensure t
   :config
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-mode +1))
 
 (use-package poetry
+  :ensure t
   :config (poetry-tracking-mode +1))
 
-(use-package pyvenv)
+(use-package pyvenv
+  :ensure t)
 
-(use-package rust-mode)
+(use-package rust-mode
+  :ensure t)
 
-(use-package lua-mode)
+(use-package lua-mode
+  :ensure t)
 
-(use-package go-mode)
+(use-package go-mode
+  :ensure t)
 
 (use-package eglot
+  :ensure t
   :config
   (add-hook 'rust-mode-hook 'eglot-ensure)
   (add-hook 'lua-mode-hook 'eglot-ensure))
 
 (use-package dap-mode
+  :ensure t
   :config
   (require 'dap-python)
   (setq dap-python-debugger 'debugpy))
 
 ;; formatting
 (use-package format-all
+  :ensure t
   :config
   (add-hook 'prog-mode-hook 'format-all-mode)
   (add-hook 'prog-mode-hook 'format-all-ensure-formatter))
+
+(use-package tree-sitter-langs
+  :ensure t
+  :config
+  (tree-sitter-require 'python)
+  (tree-sitter-require 'clojure)
+  (tree-sitter-require 'elisp)
+  (global-tree-sitter-mode))
 
 ;; COMPLETION
 (load-file "~/.emacs.d/elisp/completion-config.el")
