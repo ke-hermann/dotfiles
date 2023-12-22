ebf984db51dca79ab5c478a7affddf3ba2bc1129
Kevin Hermann <hermannkevin@gmail.com>
2023-12-22 06:24:05 +0100

    routine commit

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -14,7 +14,6 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-
 -- install plugins
 
 require("lazy").setup({
@@ -147,6 +146,18 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
+
+-- enable TAB completion and ENTER confirmation
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  })
+})
 
 -- setup servers 
 require('mason').setup({})
