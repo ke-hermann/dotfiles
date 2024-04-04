aa726925367e4b9ea85ed45539dad9c6e27cd010
Kevin Hermann <hermannkevin@gmail.com>
2024-04-04 06:46:19 +0200

    Update Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -11,5 +11,32 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
+Set-PSReadLineOption -Colors @{
+  Command            = 'Black'
+  Number             = 'DarkGray'
+  Member             = 'DarkGray'
+  Operator           = 'DarkGray'
+  Type               = 'DarkGray'
+  Variable           = 'DarkGreen'
+  Parameter          = 'DarkGreen'
+  ContinuationPrompt = 'DarkGray'
+  Default            = 'DarkGray'
+}
+
+# Logging / Progress colors
+
+$p = $host.privatedata
+$p.ErrorForegroundColor    = "Red"
+$p.ErrorBackgroundColor    = "White"
+$p.WarningForegroundColor  = "Yellow"
+$p.WarningBackgroundColor  = "White"
+$p.DebugForegroundColor    = "Yellow"
+$p.DebugBackgroundColor    = "White"
+$p.VerboseForegroundColor  = "Black"
+$p.VerboseBackgroundColor  = "White"
+$p.ProgressForegroundColor = "DarkGray"
+$p.ProgressBackgroundColor = "White"
+
+
 # zoxide setup 
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
