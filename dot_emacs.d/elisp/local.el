74518701c0a0e5f3e437ab86da79d9586ce2629f
Kevin Hermann <hermannkevin@gmail.com>
2024-04-20 18:54:17 +0200

    Add .emacs.d/elisp/local.el Add .emacs.d/elisp/utils.el

---
+++ dot_emacs.d/elisp/local.el
@@ -0,0 +1,10 @@
+;;; local.el -- file for machine specific settings that shouldn't be synced.
+
+
+;;; Commentary:
+
+;;; Code:
+(set-frame-font "Pragmata Pro 14")
+
+(provide 'local)
+;;; local.el ends here
