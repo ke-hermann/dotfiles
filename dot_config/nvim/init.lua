a518968c3999b68c049e97e5e07623c56b2da06a
Kevin Hermann <hermannkevin@gmail.com>
2024-05-01 16:20:53 +0200

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -2,14 +2,14 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -17,78 +17,87 @@ vim.opt.rtp:prepend(lazypath)
 -- install plugins
 
 require("lazy").setup({
-    -- Themes
-    {
-        "nyoom-engineering/oxocarbon.nvim",
-        config = function() end,
-    },
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-    },
-    {
-        "maxmx03/solarized.nvim",
-        lazy = false,
-        priority = 1000,
-    },
+	-- Themes
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+		config = function() end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"maxmx03/solarized.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-    { "tjdevries/colorbuddy.nvim" },
-    { 'projekt0n/github-nvim-theme' },
+	{ "tjdevries/colorbuddy.nvim" },
+	{ "projekt0n/github-nvim-theme" },
 
-    -- UX helpers
-    "folke/which-key.nvim",
-    -- File Navigation
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.4",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    -- Statusline
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-        config = function()
-        end,
-    },
-    -- File Explorer with vim keybindings
-    {
-        "stevearc/oil.nvim",
-        opts = {},
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
+	-- UX helpers
+	"folke/which-key.nvim",
+	-- File Navigation
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	-- Statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "catppuccin",
+					component_separators = { left = " ", right = " " },
+					section_separators = { left = " ", right = " " },
+				},
+			})
+		end,
+	},
+	-- File Explorer with vim keybindings
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 
-    {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        opts = {}, -- this is equalent to setup({}) function
-    },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {}, -- this is equalent to setup({}) function
+	},
 
-    -- tpope plugins
-    "tpope/vim-fugitive",
-    "tpope/vim-rhubarb",
-    "tpope/vim-surround",
-    "tpope/vim-sleuth",
-    "tpope/vim-dispatch",
-    "Olical/conjure",
-    "clojure-vim/vim-jack-in",
-    "nvim-treesitter/nvim-treesitter",
+	-- tpope plugins
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
+	"tpope/vim-surround",
+	"tpope/vim-sleuth",
+	"tpope/vim-dispatch",
+	"Olical/conjure",
+	"clojure-vim/vim-jack-in",
+	"nvim-treesitter/nvim-treesitter",
 
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
+	{ "williamboman/mason.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
 
-    { "VonHeikemen/lsp-zero.nvim",        branch = "v3.x" },
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/nvim-cmp" },
-    { "L3MON4D3/LuaSnip" },
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "L3MON4D3/LuaSnip" },
 
-    {
-        "stevearc/conform.nvim",
-        opts = {},
-    },
-    { "tomasky/bookmarks.nvim" },
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	{ "tomasky/bookmarks.nvim" },
 })
 
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -138,8 +147,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.opt.termguicolors = true
 
 vim.opt.background = "dark"
-vim.cmd.colorscheme("github_dark_default")
-
+vim.cmd.colorscheme("catppuccin-mocha")
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
@@ -178,69 +186,58 @@ vim.keymap.set("t", "jk", [[<C-\><C-n>]]) -- normal mode mapping for term emulat
 vim.keymap.set("n", "<leader>ex", ":Ex %:p:h<CR>", { desc = "open file explorer" })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
-
 -- Treesitter config
 --
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "python", "c", "cpp", "lua", "clojure", "go" },
-    highlight = {
-        enable = true,
-        use_languagetree = true,
-    },
-    indent = {
-        enable = true,
-        prefer_gk_over_autocmd = true,
-    },
-    cintegration = {
-        enable = true,
-        highlight_ignores = { "Bar", "Comment" },
-    },
-    autopairs = {
-        enable = true,
-    },
+	ensure_installed = { "python", "c", "cpp", "lua", "clojure", "go" },
+	highlight = {
+		enable = true,
+		use_languagetree = true,
+	},
+	indent = {
+		enable = true,
+		prefer_gk_over_autocmd = true,
+	},
+	cintegration = {
+		enable = true,
+		highlight_ignores = { "Bar", "Comment" },
+	},
+	autopairs = {
+		enable = true,
+	},
 })
 
 -- LSP Config
 local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-    -- see :help lsp-zero-keybindings
-    -- to learn the available actions
-    lsp_zero.default_keymaps({ buffer = bufnr })
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
 require("mason").setup({})
 require("mason-lspconfig").setup({
-    ensure_installed = { "pyright", "lua_ls", "gopls" },
-    handlers = {
-        lsp_zero.default_setup,
-    },
+	ensure_installed = { "pyright", "lua_ls", "gopls" },
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
 
 require("conform").setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
-        -- Use a sub-list to run only the first available formatter
-        javascript = { { "prettierd", "prettier" } },
-        go = { "goimports", "gofmt" },
-    },
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_fallback = true,
-    },
-})
-
-
-require("lualine").setup({
-    options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = { left = " ", right = " " },
-        section_separators = { left = " ", right = " " },
-    },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- Use a sub-list to run only the first available formatter
+		javascript = { { "prettierd", "prettier" } },
+		go = { "goimports", "gofmt" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 })
