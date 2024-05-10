b4a0fbb11965088e947c5f9ff3655c0e984e14f8
ke-hermann <110266080+ke-hermann@users.noreply.github.com>
2024-05-10 17:25:40 +0200

    updated fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,10 +2,10 @@
 starship init fish | source
 
 # opam configuration
-source /home/kevin/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 zoxide init --cmd cd fish | source
 # improved and synced shell history completion
 atuin init fish | source
 
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/kevinh/.ghcup/bin $PATH # ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH # ghcup-env
