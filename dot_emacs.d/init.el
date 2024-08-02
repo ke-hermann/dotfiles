1505f7020c625a96b692de8afc4132a9af0deffd
ke-hermann <110266080+ke-hermann@users.noreply.github.com>
2024-08-02 16:57:18 +0200

    added meow config

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -87,25 +87,14 @@
 ;; PACKAGES ;;
 ;;;;;;;;;;;;;;
 
-(use-package solarized-theme
-  :ensure t
-  :config (load-theme 'solarized-dark t))
+(use-package solarized-theme :ensure t )
 
-(use-package evil
-  :ensure t
-  :custom
-  (evil-undo-system 'undo-redo)
-  (evil-want-keybinding nil)
-  (evil-want-C-u-scroll 1)
-  :config (evil-mode 1))
+(use-package catppuccin-theme
+  :ensure t)
 
-(use-package evil-collection
-  :after evil
+(use-package doom-themes
   :ensure t
-  :config
-  (evil-collection-init))
-
-(use-package doom-themes :ensure t)
+  :config (load-theme 'doom-tokyo-night))
 
 (use-package ef-themes :ensure t)
 
@@ -308,9 +297,6 @@
 (use-package cider
   :ensure t)
 
-(use-package evil-paredit
-  :ensure t
-  :init (add-hook 'paredit-mode-hook #'evil-paredit-mode))
 
 (use-package projectile
   :ensure t
@@ -326,18 +312,114 @@
     :ensure t)
   :config
   (add-hook 'python-mode-hook 'auto-virtualenv-set-virtualenv)
-  (add-hook 'projectile-after-switch-project-hook 'auto-virtualenv-set-virtualenv) ;; If using projectile
-  )
+  (add-hook 'projectile-after-switch-project-hook 'auto-virtualenv-set-virtualenv)) ;; If using projectile)
+
+(use-package meow
+  :ensure t
+  :init
+  (defun meow-setup ()
+    (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+    (meow-define-keys
+	'insert
+      '("C-]" . meow-insert-exit))
+    (meow-motion-overwrite-define-key
+     '("j" . meow-next)
+     '("k" . meow-prev)
+     '("<escape>" . ignore))
+    (meow-leader-define-key
+     ;; SPC j/k will run the original command in MOTION state.
+     '("j" . "H-j")
+     '("k" . "H-k")
+     ;; Use SPC (0-9) for digit arguments.
+     '("1" . meow-digit-argument)
+     '("2" . meow-digit-argument)
+     '("3" . meow-digit-argument)
+     '("4" . meow-digit-argument)
+     '("5" . meow-digit-argument)
+     '("6" . meow-digit-argument)
+     '("7" . meow-digit-argument)
+     '("8" . meow-digit-argument)
+     '("9" . meow-digit-argument)
+     '("0" . meow-digit-argument)
+     '("/" . meow-keypad-describe-key)
+     '("?" . meow-cheatsheet))
+    (meow-normal-define-key
+     '("0" . meow-expand-0)
+     '("9" . meow-expand-9)
+     '("8" . meow-expand-8)
+     '("7" . meow-expand-7)
+     '("6" . meow-expand-6)
+     '("5" . meow-expand-5)
+     '("4" . meow-expand-4)
+     '("3" . meow-expand-3)
+     '("2" . meow-expand-2)
+     '("1" . meow-expand-1)
+     '("-" . negative-argument)
+     '(";" . meow-reverse)
+     '("," . meow-inner-of-thing)
+     '("." . meow-bounds-of-thing)
+     '("[" . meow-beginning-of-thing)
+     '("]" . meow-end-of-thing)
+     '("a" . meow-append)
+     '("A" . meow-open-below)
+     '("b" . meow-back-word)
+     '("B" . meow-back-symbol)
+     '("c" . meow-change)
+     '("d" . meow-delete)
+     '("D" . meow-backward-delete)
+     '("e" . meow-next-word)
+     '("E" . meow-next-symbol)
+     '("f" . meow-find)
+     '("g" . meow-cancel-selection)
+     '("G" . meow-grab)
+     '("h" . meow-left)
+     '("H" . meow-left-expand)
+     '("i" . meow-insert)
+     '("I" . meow-open-above)
+     '("j" . meow-next)
+     '("J" . meow-next-expand)
+     '("k" . meow-prev)
+     '("K" . meow-prev-expand)
+     '("l" . meow-right)
+     '("L" . meow-right-expand)
+     '("m" . meow-join)
+     '("n" . meow-search)
+     '("o" . meow-block)
+     '("O" . meow-to-block)
+     '("p" . meow-yank)
+     '("q" . meow-quit)
+     '("Q" . meow-goto-line)
+     '("r" . meow-replace)
+     '("R" . meow-swap-grab)
+     '("s" . meow-kill)
+     '("t" . meow-till)
+     '("u" . meow-undo)
+     '("U" . meow-undo-in-selection)
+     '("v" . meow-visit)
+     '("w" . meow-mark-word)
+     '("W" . meow-mark-symbol)
+     '("x" . meow-line)
+     '("X" . meow-goto-line)
+     '("y" . meow-save)
+     '("Y" . meow-sync-grab)
+     '("z" . meow-pop-selection)
+     '("'" . repeat)
+     '("<escape>" . ignore)))
+  :config
+  (require 'meow)
+  (meow-setup)
+  (meow-global-mode 1))
 
 ;;;;;;;;;;;;;
 ;; KEYMAPS ;;
 ;;;;;;;;;;;;;
 
+(global-set-key (kbd "<f5>") 'consult-theme)
 (global-set-key (kbd "<f6>") (lambda () (interactive) (consult-theme 'solarized-light)))
 (global-set-key (kbd "<f7>") (lambda () (interactive) (consult-theme 'solarized-dark)))
 (global-set-key (kbd "<f2>") 'menu-bar-mode)
-(global-set-key (kbd "M-n") 'scroll-up)
-(global-set-key (kbd "M-p") 'scroll-down)
+(global-set-key (kbd "M-p") 'meow-page-up)
+(global-set-key (kbd "M-n") 'meow-page-down)
 (global-set-key (kbd "M-f") 'forward-to-word)
 (global-set-key (kbd "C-x C-n") 'company-complete)
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
