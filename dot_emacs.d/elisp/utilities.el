dc71831120354504b39461d769abf9b7f3200ac8
Kevin Hermann <hermannkevin@gmail.com>
2025-07-26 20:18:31 +0200

    added: elisp/utilities

---
+++ dot_emacs.d/elisp/utilities.el
@@ -0,0 +1,25 @@
+(defun org-insert-file-task-list (directory)
+  "Insert an Org-mode task list of all files in DIRECTORY into the current buffer."
+  (interactive "DDirectory: ")
+  (let ((files (directory-files directory t "^[^.].*")))
+    (dolist (file files)
+      (when (file-regular-p file)
+        (insert (format "- [ ] %s\n" (file-name-nondirectory file)))))))
+
+(defun org-insert-formatted-link (url)
+  "Insert an Org-mode link for URL with the webpage title as description."
+  (interactive "sURL: ")
+  (let* ((title (org-insert-formatted-link--get-title url))
+         (org-link (format "[[%s][%s]]" url (or title url))))
+    (insert org-link)))
+
+(defun org-insert-formatted-link--get-title (url)
+  "Helper function to extract title from URL."
+  (require 'url)
+  (require 'dom)
+  (with-temp-buffer
+    (url-insert-file-contents url)
+    (let ((dom (libxml-parse-html-region (point-min) (point-max))))
+      (dom-text (dom-by-tag dom 'title)))))
+
+(provide 'utilities)
