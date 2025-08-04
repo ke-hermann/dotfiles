039cd6a30ec349871b50220bb8ff96ab8189826c
Kevin Hermann <hermannkevin@gmail.com>
2025-08-05 01:50:35 +0200

    updated nvim; nushell'

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,70 +1,53 @@
-require 'config.lazy'
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.termguicolors = true
+vim.o.wrap = false
+vim.o.tabstop = 4
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+vim.o.clipboard = "unnamedplus"
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
--- Make line numbers default
-vim.opt.number = true
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
--- Don't show the mode, since it's already in status line
-vim.opt.showmode = false
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
--- Enable break indent
-vim.opt.breakindent = true
--- Save undo history
-vim.opt.undofile = true
--- Case-insensitive searching UNLESS \C or capital in search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
--- Decrease update time
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
--- Sets how neovim will display certain whitespace in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
--- Show which line your cursor is on
-vim.opt.cursorline = true
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
 
--- Set highlight on search, but clear on pressing <Esc> in normal mode
-vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
--- 24 bit colors
-vim.opt.termguicolors = true
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
--- General Keymaps
-vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set('t', 'jk', [[<C-\><C-n>]]) -- normal mode mapping for term emulator
-vim.keymap.set('n', '<leader>ex', ':Oil %:p:h<CR>', { desc = 'open file explorer' })
--- other custom keybindings
-vim.keymap.set('n', '<leader>bh', '<CMD>browse oldfiles<CR>', { desc = 'browse history' })
-vim.keymap.set('n', '<leader>bd', '<CMD>bdelete<CR>', {})
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.pack.add({
+	{ src = "https://github.com/vague2k/vague.nvim" },
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
 })
 
--- LSP CONFIG
-vim.lsp.enable { 'pyright', 'lua_ls', 'dartls', 'zls' }
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
+
+require "mini.pick".setup()
+require "nvim-treesitter.configs".setup({
+	ensure_installed = { "lua", "python", "clojure", "rust", "haskell", "cpp" },
+	highlight = { enable = true }
+})
+require "oil".setup()
+
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>b', ":Pick buffers<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.lsp.enable({ "lua_ls", "biome", "tinymist", "emmetls" })
+
+require "vague".setup({ transparent = true })
+vim.cmd("colorscheme vague")
--- private_dot_config/nvim/lua/plugins/tpope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { 'tpope/vim-fugitive' }, -- Git wrapper
-  { 'tpope/vim-surround' }, -- Operate on surroundings (quotes, parens, etc.)
-  { 'tpope/vim-commentary' }, -- Easy commenting and uncommenting
-  { 'tpope/vim-unimpaired' }, -- Useful mappings like `]`o` for new line below
-  { 'tpope/vim-repeat' }, -- Enable repeating complex commands with `.`
-  { 'tpope/vim-abolish' }, -- Word transformations and substitutions
-  { 'tpope/vim-sleuth' }, -- Auto-detect indentation settings
-}
