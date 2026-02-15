139a4deb1e10013190de3764103543616dd9946e
Kevin Hermann <hermannkevin@gmail.com>
2026-02-15 04:27:10 +0100

    UPDATED: wezterm, alacritty, nvim

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,71 +1,50 @@
 -- Pull in  wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-local is_windows = wezterm.target_triple == 'x86_64-pc-windows-msvc'
+local is_windows = wezterm.target_triple == "x86_64-pc-windows-msvc"
 
 -- or, changing the font size and color scheme.
-config.font = wezterm.font "CaskaydiaCove NF"
+config.font = wezterm.font("CaskaydiaCove NF")
 config.font_size = 13
 -- smart tab bar behavior
 config.hide_tab_bar_if_only_one_tab = true
 
 if is_windows then
-  -- Adjust path/flags if you installed Nu elsewhere
-  config.default_prog = { 'nu.exe' }
+	-- Adjust path/flags if you installed Nu elsewhere
+	config.default_prog = { "nu.exe" }
 end
 
 -- Use CTRL+W as a "leader" key, similar to Vim
 config.leader = {
-  key = 'q',
-  mods = 'CTRL',
-  timeout_milliseconds = 1000,
+	key = "q",
+	mods = "CTRL",
+	timeout_milliseconds = 1000,
 }
 
 config.keys = config.keys or {}
 local function map(mods, key, action)
-  table.insert(config.keys, { mods = mods, key = key, action = action })
+	table.insert(config.keys, { mods = mods, key = key, action = action })
 end
 
 -- splits
-map('LEADER', 'v', act.SplitHorizontal { domain = 'CurrentPaneDomain' })
-map('LEADER', 'h', act.SplitVertical { domain = 'CurrentPaneDomain' })
+map("LEADER", "v", act.SplitHorizontal({ domain = "CurrentPaneDomain" }))
+map("LEADER", "h", act.SplitVertical({ domain = "CurrentPaneDomain" }))
 -- Vim-style pane navigation with Alt+h/j/k/l
-map('ALT', 'h', act.ActivatePaneDirection 'Left')
-map('ALT', 'j', act.ActivatePaneDirection 'Down')
-map('ALT', 'k', act.ActivatePaneDirection 'Up')
-map('ALT', 'l', act.ActivatePaneDirection 'Right')
+map("ALT", "h", act.ActivatePaneDirection("Left"))
+map("ALT", "j", act.ActivatePaneDirection("Down"))
+map("ALT", "k", act.ActivatePaneDirection("Up"))
+map("ALT", "l", act.ActivatePaneDirection("Right"))
 
--- scholarized theme, following the original spec
-config.colors = {
-  foreground = '#839496', -- base0
-  background = '#002b36', -- base03
+local canonical_solarized = require("canonical_solarized")
+canonical_solarized.apply_to_config(config)
 
-  ansi = {
-    '#073642', -- base02 (black)
-    '#dc322f', -- red
-    '#859900', -- green
-    '#b58900', -- yellow
-    '#268bd2', -- blue
-    '#d33682', -- magenta
-    '#2aa198', -- cyan
-    '#eee8d5', -- base2 (white)
-  },
-
-  brights = {
-    '#002b36', -- base03 (bright black)
-    '#cb4b16', -- orange (bright red)
-    '#586e75', -- base01 (bright green)
-    '#657b83', -- base00 (bright yellow)
-    '#839496', -- base0 (bright blue)
-    '#6c71c4', -- violet (bright magenta)
-    '#93a1a1', -- base1 (bright cyan)
-    '#fdf6e3', -- base3 (bright white)
-  },
-  }
+-- config.color_scheme = "Canonical Solarized Dark"
+-- config.color_scheme = "rose-pine"
+config.color_scheme = "Solarized (dark) (terminal.sexy)"
 
 -- Finally, return the configuration to wezterm:
 return config
--- private_dot_config/nvim/lua/plugins/themes.lua
+++ private_dot_config/nvim/lua/plugins/themes.lua
@@ -1,6 +1,7 @@
 return {
-	{ "catppuccin/nvim", },
-	{ "folke/tokyonight.nvim", },
-	{ "nyoom-engineering/oxocarbon.nvim"},
-	{ 'maxmx03/solarized.nvim', }
+	{ "catppuccin/nvim" },
+	{ "folke/tokyonight.nvim" },
+	{ "nyoom-engineering/oxocarbon.nvim" },
+	{ "maxmx03/solarized.nvim" },
+	{ "rose-pine/neovim", name = "rose-pine" },
 }
