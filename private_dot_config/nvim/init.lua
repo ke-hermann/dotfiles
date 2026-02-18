156985fb9a5b22940588f66b0791985ae2987143
Kevin Hermann <hermannkevin@gmail.com>
2026-02-18 10:24:29 +0100

    simplified neovim config

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,7 +1,283 @@
-require("base")
-require("config.lazy")
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.o.termguicolors = true
-vim.o.background = 'dark'
-require('solarized').setup(opts)
-vim.cmd.colorscheme 'solarized'
+-- In insert mode, typing jk quickly will leave to normal mode
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true })
+
+-- Make line numbers default
+vim.o.number = true
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = "a"
+vim.o.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+vim.schedule(function()
+  vim.o.clipboard = "unnamedplus"
+end)
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = "yes"
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+vim.o.confirm = true
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- Exit terminal mode in the builtin terminal
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.hl.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- plugins
+    {
+      "williamboman/mason.nvim",
+      config = function()
+        -- Mason setup
+        require("mason").setup()
+      end,
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      dependencies = { "mason.nvim" },
+      config = function()
+        -- Automatically ensure LSP servers are installed
+        require("mason-lspconfig").setup({
+          ensure_installed = { "pyright", "lua_ls" }, -- Add your preferred LSP servers
+        })
+      end,
+    },
+    {
+      "neovim/nvim-lspconfig",
+      dependencies = { "mason-lspconfig.nvim" },
+      config = function()
+        -- LSP setup
+        local lspconfig = require("lspconfig")
+
+        -- Basic LSP server setup
+        vim.lsp.enable("pyright")
+        vim.lsp.enable("lua-language-server")
+      end,
+    },
+    {
+      "hrsh7th/nvim-cmp", -- Completion plugin (useful with LSP)
+      config = function()
+        local cmp = require("cmp")
+        cmp.setup({
+          sources = {
+            { name = "nvim_lsp" },
+          },
+        })
+      end,
+    },
+    {
+      "hrsh7th/cmp-nvim-lsp", -- Completion source for LSP
+    },
+    {
+      "L3MON4D3/LuaSnip", -- Snippet engine (optional)
+    },
+
+    {
+      "nvim-lualine/lualine.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      config = function()
+        require("lualine").setup({
+          options = {
+            icons_enabled = true,
+            theme = "solarized",
+          },
+        })
+      end,
+    },
+
+    {
+      "stevearc/conform.nvim",
+      opts = {},
+      config = function()
+        require("conform").setup({
+          formatters_by_ft = {
+            lua = { "stylua" },
+            -- Conform will run multiple formatters sequentially
+            python = { "isort", "black" },
+            -- You can customize some of the format options for the filetype (:help conform.format)
+            rust = { "rustfmt", lsp_format = "fallback" },
+            -- Conform will run the first available formatter
+            javascript = { "prettierd", "prettier", stop_after_first = true },
+          },
+          format_on_save = {
+            -- These options will be passed to conform.format()
+            timeout_ms = 500,
+            lsp_format = "fallback",
+          },
+        })
+      end,
+    },
+
+    {
+      "stevearc/oil.nvim",
+      ---@module 'oil'
+      ---@type oil.SetupOpts
+      opts = {},
+      -- Optional dependencies
+      dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+      -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+      -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+      lazy = false,
+      config = function()
+        require("oil").setup()
+        vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+      end,
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      tag = "v0.2.0",
+      dependencies = { "nvim-lua/plenary.nvim" },
+
+      config = function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Telescope buffers" })
+        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+        vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+        vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+      end,
+    },
+    {
+      { "catppuccin/nvim" },
+      { "folke/tokyonight.nvim" },
+      { "nyoom-engineering/oxocarbon.nvim" },
+      { "maxmx03/solarized.nvim" },
+      { "rose-pine/neovim", name = "rose-pine" },
+    },
+    {
+      "folke/trouble.nvim",
+      opts = {}, -- for default options, refer to the configuration section for custom setup.
+      cmd = "Trouble",
+      keys = {
+        {
+          "<leader>xx",
+          "<cmd>Trouble diagnostics toggle<cr>",
+          desc = "Diagnostics (Trouble)",
+        },
+        {
+          "<leader>xX",
+          "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+          desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+          "<leader>cs",
+          "<cmd>Trouble symbols toggle focus=false<cr>",
+          desc = "Symbols (Trouble)",
+        },
+        {
+          "<leader>cl",
+          "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+          desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+          "<leader>xL",
+          "<cmd>Trouble loclist toggle<cr>",
+          desc = "Location List (Trouble)",
+        },
+        {
+          "<leader>xQ",
+          "<cmd>Trouble qflist toggle<cr>",
+          desc = "Quickfix List (Trouble)",
+        },
+      },
+    },
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      keys = {
+        {
+          "<leader>?",
+          function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
+    },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "solarized" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
--- private_dot_config/nvim/lua/plugins/themes.lua
+++
@@ -1,7 +0,0 @@
-return {
-	{ "catppuccin/nvim" },
-	{ "folke/tokyonight.nvim" },
-	{ "nyoom-engineering/oxocarbon.nvim" },
-	{ "maxmx03/solarized.nvim" },
-	{ "rose-pine/neovim", name = "rose-pine" },
-}
